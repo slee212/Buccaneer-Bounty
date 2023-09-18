9144d4f71e88744d2a73bfc238f664c4087c7422
Christiaan <small.christiaan@gmail.com>
2023-09-18 23:08:45 +1000

    Added Breeze Sound Effect

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class ShipMovement : MonoBehaviour
@@ -17,14 +18,30 @@ public class ShipMovement : MonoBehaviour
     public float emissionBuildUpSpeed = 10f;
     private float currentEmissionRate = 0f;
 
+    public AudioSource audioSource; // Drag your AudioSource component here
+    public AudioClip boostSound; // Drag your boost sound here
+    public float fadeDuration = 1.0f; // Duration for fade in/out
+    private bool isBoosting = false;
+    private Coroutine fadeCoroutine;
+
+    public ParticleSystem waterSplashEffect; // Drag your water splash effect here
+    private ParticleSystem.MainModule mainModule;
+
     void Start()
     {
         mainCamera.enabled = true;
         secondaryCamera.enabled = false;
         mainCamera.fieldOfView = normalFOV;
 
-        // Initialize emission module
+        // Initialize emission module for wind effect
         emissionModule = windEffect.emission;
+
+        // Initialize audio
+        audioSource.clip = boostSound;
+        audioSource.volume = 0f;
+
+        // Initialize water splash main module
+        mainModule = waterSplashEffect.main;
     }
 
     void Update()
@@ -37,7 +54,7 @@ public class ShipMovement : MonoBehaviour
             currentSpeed = boostedSpeed;
             targetFOV = boostedFOV;
 
-            // Increase emission rate over time
+            // Increase emission rate over time for wind effect
             currentEmissionRate = Mathf.Min(currentEmissionRate + emissionBuildUpSpeed * Time.deltaTime, maxEmissionRate);
             emissionModule.rateOverTime = currentEmissionRate;
 
@@ -46,18 +63,59 @@ public class ShipMovement : MonoBehaviour
             {
                 windEffect.Play();
             }
+
+            // Check for boost
+            if (!isBoosting)
+            {
+                isBoosting = true;
+                if (fadeCoroutine != null)
+                {
+                    StopCoroutine(fadeCoroutine);
+                }
+                fadeCoroutine = StartCoroutine(FadeInAudio());
+            }
         }
         else
         {
-            // Reset emission rate
+            // Reset emission rate for wind effect
             currentEmissionRate = 0f;
             emissionModule.rateOverTime = currentEmissionRate;
 
             // Stop the wind effect
             windEffect.Stop();
+
+            // Check for boost end
+            if (isBoosting)
+            {
+                isBoosting = false;
+                if (fadeCoroutine != null)
+                {
+                    StopCoroutine(fadeCoroutine);
+                }
+                fadeCoroutine = StartCoroutine(FadeOutAudio());
+            }
         }
 
         mainCamera.fieldOfView = Mathf.Lerp(mainCamera.fieldOfView, targetFOV, fovTransitionSpeed * Time.deltaTime);
+
+        // Adjust water splash effect
+        ParticleSystem.EmissionModule splashEmissionModule = waterSplashEffect.emission;
+        if (Input.GetKey(KeyCode.W))
+        {
+            splashEmissionModule.rateOverTime = 60f;
+            if (isBoosting)
+            {
+                mainModule.startLifetime = 0.7f;
+            }
+            else
+            {
+                mainModule.startLifetime = 0.3f;
+            }
+        }
+        else
+        {
+            splashEmissionModule.rateOverTime = 0f;
+        }
 
         if (Input.GetKey(KeyCode.W))
         {
@@ -82,5 +140,36 @@ public class ShipMovement : MonoBehaviour
             mainCamera.enabled = !mainCamera.enabled;
             secondaryCamera.enabled = !secondaryCamera.enabled;
         }
+    }
+
+    IEnumerator FadeInAudio()
+    {
+        audioSource.Play();
+        float startVolume = 0f;
+        float endVolume = 0.25f;
+        float currentTime = 0;
+
+        while (currentTime < fadeDuration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(startVolume, endVolume, currentTime / fadeDuration);
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOutAudio()
+    {
+        float startVolume = 0.25f;
+        float endVolume = 0f;
+        float currentTime = 0;
+
+        while (currentTime < fadeDuration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(startVolume, endVolume, currentTime / fadeDuration);
+            yield return null;
+        }
+
+        audioSource.Stop();
     }
 }
