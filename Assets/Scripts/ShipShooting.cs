6a8bd3fbdfde700a84d204527c4c44f229d7a444
Christiaan <small.christiaan@gmail.com>
2023-09-18 22:07:34 +1000

    Added visuals + Sounds to the Cannon

--- Assets/Scripts/ShipShooting.cs
+++ Assets/Scripts/ShipShooting.cs
@@ -6,6 +6,9 @@ public class ShipShooting : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform bulletSpawnPoint;
     public float bulletSpeed = 20.0f;
+    public GameObject explosionEffect; // Drag your explosion prefab here
+    public AudioSource audioSource; // Drag your AudioSource component here
+    public AudioClip explosionSound; // Drag your explosion sound here
 
     void Update()
     {
@@ -17,12 +20,19 @@ public class ShipShooting : MonoBehaviour
 
     void Shoot()
     {
+        // Instantiate bullet and apply force
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
-
         if (rb != null)
         {
             rb.AddForce(bullet.transform.forward * bulletSpeed, ForceMode.Impulse);
         }
+
+        // Instantiate explosion effect
+        GameObject explosion = Instantiate(explosionEffect, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        Destroy(explosion, 2.0f); // Destroy the explosion effect after 2 seconds
+
+        // Play explosion sound
+        audioSource.PlayOneShot(explosionSound);
     }
 }
