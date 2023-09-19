f56667975a37fd9d6f1f34edb019e423007c9b15
slee212 <samuellee249@gmail.com>
2023-09-19 19:02:46 +1000

    Added shop ui

---
+++ Assets/Scripts/ShopTrigger.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject shopUI;
+    private bool isPlayerInRange = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.E)) {
+            ToggleShopUI();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("In range");
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+            // shopPromptUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            isPlayerInRange = false;
+            // shopPromptUI.SetActive(false);
+            shopUI.SetActive(false);
+        }
+    }
+
+    private void ToggleShopUI() {
+        shopUI.SetActive(!shopUI.activeSelf);
+    }
+}
