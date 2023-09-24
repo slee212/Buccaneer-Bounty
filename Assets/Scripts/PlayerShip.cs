c08fcf64faa7e0a00f10e5a0eb817eb2ca77d28e
slee212 <samuellee249@gmail.com>
2023-09-24 19:47:18 +1000

    Added health bar
    Health bar is reflective of the current health and changes from green to red when below 25%.

--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -1,9 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerShip : MonoBehaviour
 {
+    public Image healthbar;
+    private float originalHealthBarWidth;
     public int health = 100;
+    public int maxHealth;
+
+    void Start()
+    {
+        maxHealth = health;
+
+        if (healthbar != null)
+        {
+            originalHealthBarWidth = healthbar.rectTransform.rect.width;
+        }
+    }
 
     public void LoadGame(string sceneName)
     {
@@ -14,6 +28,21 @@ public class PlayerShip : MonoBehaviour
     {
         health -= damage;
         Debug.Log("Player hit! Current health: " + health);
+
+        if (healthbar != null)
+        {
+            float currentHealthPercentage = (float)health / maxHealth;
+            float newWidth = originalHealthBarWidth * currentHealthPercentage;
+            float currentHeight = healthbar.rectTransform.rect.height;
+            healthbar.rectTransform.sizeDelta = new Vector2(newWidth, currentHeight);
+            if (currentHealthPercentage <= 0.25f)
+            {
+                healthbar.color = new Color(255f / 255f, 88f / 255f, 94f / 255f, 1f); // Red
+            } else
+            {
+                healthbar.color = new Color(134f / 255f, 255f / 255f, 141f / 255f, 1f); // Green
+            }
+        }
 
         if (health <= 0)
         {
