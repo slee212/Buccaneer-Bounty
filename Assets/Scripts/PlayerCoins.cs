3f3f3d091be67bc8120074cf63df08c6218308d2
slee212 <samuellee249@gmail.com>
2023-09-27 21:20:44 +1000

    Update PlayerCoins.cs
    Player Coins now update regardless of ship level.

--- Assets/Scripts/PlayerCoins.cs
+++ Assets/Scripts/PlayerCoins.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class PlayerCoins : MonoBehaviour
@@ -13,23 +12,36 @@ public class PlayerCoins : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Attempt to automatically find the coinText object by tag
+        GameObject coinTextObj = GameObject.FindWithTag("coinCounter");
+        if (coinTextObj != null)
+        {
+            coinText = coinTextObj.GetComponent<TextMeshProUGUI>();
+        }
+        else
+        {
+            Debug.LogError("Object with tag 'CoinText' not found.");
+        }
+
         UpdateCoinUI();
     }
+
     public void AddCoin()
     {
-            Debug.Log("AddCoin method called!");  // Debug lo
+        Debug.Log("AddCoin method called!");
         coins += 1;
         Debug.Log("Coin collected! Current coins: " + coins);
         UpdateCoinUI();
     }
-    // Update is called once per frame
+
     public void SpendCoins(int amount)
     {
         if (coins >= amount)
         {
             coins -= amount;
             UpdateCoinUI();
-        } else
+        } 
+        else
         {
             Debug.Log("Not enough coins");
         }
@@ -37,8 +49,13 @@ public class PlayerCoins : MonoBehaviour
 
     private void UpdateCoinUI()
     {
-        if (coinText){
+        if (coinText)
+        {
             coinText.text = coins.ToString();
+        }
+        else
+        {
+            Debug.LogError("coinText is null. Please assign the TextMeshProUGUI component in the Inspector, or make sure the object with tag 'CoinText' has a TextMeshProUGUI component.");
         }
     }
 }
